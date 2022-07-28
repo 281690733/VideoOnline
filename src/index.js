8dd533217bb36ed99f9e75507c00d64225dc448c
281690733 <61697228+281690733@users.noreply.github.com>
2022-07-28 10:10:14 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+/// <reference types="vite/client" />
+
+import styles from "./style.css?inline";
+
+/**
+ * Register it before joining room:
+ * ```js
+ * WindowManager.register({
+ *   kind: "Counter",
+ *   src: Counter
+ * })
+ * ```
+ * Then you can use it in your room:
+ * ```js
+ * manager.addApp({ kind: 'Counter' })
+ * ```
+ * Read more about how to make a netless app here:
+ * https://github.com/netless-io/window-manager/blob/master/docs/develop-app.md
+ *
+ * @type {import("@netless/window-manager").NetlessApp}
+ */
+const Counter = {
+  kind: "Counter",
+  setup(context) {
+    const box = context.getBox();
+    box.mountStyles(styles);
+
+    const $content = document.createElement("div");
+    $content.className = "app-counter";
+    box.mountContent($content);
+
+    const $button = document.createElement("button");
+    $content.appendChild($button);
+
+    const storage = context.createStorage("counter", { count: 0 });
+    $button.onclick = ev => {
+      storage.setState({ count: storage.state.count + (ev.shiftKey ? -1 : 1) });
+    };
+
+    function refresh() {
+      $button.textContent = String(storage.state.count);
+    }
+    const dispose = storage.addStateChangedListener(refresh);
+    refresh();
+
+    context.emitter.on("destroy", () => {
+      dispose();
+    });
+  },
+};
+
+export default Counter;
